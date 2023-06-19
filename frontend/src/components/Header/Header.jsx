3b95c277624daf6e2cebf83bd0d614ca7a92a479
Zano <zanopcwr@protonmail.com>
2023-06-19 11:24:56 -0300

    basic front components (header, main, footer)

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+export default function Header() {
+    return(
+        <header class="header" id="main-page-header">
+            <a href="#"> HOME </a>
+            <a href="#"> HOME 2 </a>
+            <a href="#"> HOME 3< /a>
+        </header>
+    )
+}
---
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header"
+import Main from "../../components/Main/Main"
+import Footer from "../../components/Footer/Footer"
+
+export default function MainPage() {
+    return(
+        <div id="main-page-body">
+            <Header id="header" />
+            <Main id="main" />
+            <Footer id="footer" />
+        </div>
+    )
+}
