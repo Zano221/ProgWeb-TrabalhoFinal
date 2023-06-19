52fe61258fddd4b2169253bf1ec554d4b95099c7
Zano <zanopcwr@protonmail.com>
2023-06-19 17:40:23 -0300

    database and victim pge

---
+++ frontend/src/pages/VitimaPage/VitimaPage.jsx
@@ -0,0 +1,19 @@
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer"
+import './VitimaPage.css'
+
+
+
+export default function VitimaPage() {
+
+
+    return(
+        <section id="vitima-page">
+            <Header />
+                <h1>Lista de Vitimas</h1>
+                
+                {}
+            <Footer />
+        </section>
+    )   
+}
